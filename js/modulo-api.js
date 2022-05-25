5a54a5acfb0ad7f7e6db1e6ca070fe2788862993
Marco Corbetta <machopper97@gmail.com>
2022-05-25 14:34:42 +0200

    pulizia codice

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -1,8 +1,10 @@
-export const URL="http://www.omdbapi.com/?i=tt3896198&apikey=4bc01fe7&t=terminator";
-export const URL2="http://www.omdbapi.com/?i=tt3896198&apikey=4bc01fe7&s=terminator&type=series";
+/*export const URL="http://www.omdbapi.com/?i=tt3896198&apikey=4bc01fe7&t=terminator";
+export const URL2="http://www.omdbapi.com/?i=tt3896198&apikey=4bc01fe7&s=terminator&type=series";*/
+
+import{BASE_URL} from "./config.js";
 
 
-export const film=() =>{
+/*export const film=() =>{
     fetch(URL)
         .then((response)=>response.json())
         .then((results)=>{
@@ -18,6 +20,17 @@ export const serie=() =>{
         .then((results)=>{
             const series=results.Search;
             console.table(series);
+        });
+       
+};*/
+
+export const apiList =(s,type) =>{
+    const url= BASE_URL + `s=${s}&type=${type}`;
+    fetch(url)
+        .then((response)=>response.json())
+        .then((results)=>{
+            const items=results.Search;
+            console.table(items);
         });
        
 };
