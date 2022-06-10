9358481ff183364b95905dec1dfa4d63a3c74edd
Marco Corbetta <machopper97@gmail.com>
2022-06-10 19:13:04 +0200

    Aggiunto pop-up

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const API_KEY="4bc01fe7";
-export const BASE_URL=`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&`;
+export const BASE_URL=`https://www.omdbapi.com/?&apikey=${API_KEY}&`;
 
 export const CONTENT_TYPE={
     MOVIE:"movie",
--- js/modulo-api.js
+++ js/modulo-api.js
@@ -84,25 +84,62 @@ import {CONTENT_TYPE} from "../js/config.js";
 
 
 
+/*apiList per il singolo film*/
+
+/*export const apiListFilm = (s1) => {
+  const url1 = BASE_URL + "i=" + s1 + "&plot=short" ;
+  fetch(url1)
+  .then((response1)=>response1.json())
+  .then((results1)=>{
+    const items1=results1;
+      
+      stampa(items1);
+      
+    });
+};*/
+
+
+   /* const stampa= async (items1)=>{
+      
+      return  await items1.Plot;
+    }*/
+   
+        
+       
+
+ /* const viewItemsFilm =  (items) => {
+    items.map((item) => {
+     
+      const arr=[item.Cast,item.Plot];
+
+     return arr;
+    
+
+    });
+  };*/
+
+/***************************************************** */
+
+
+
+export const apiList = async (s, type) => {
+    const url = BASE_URL + "s=" + s + "&type=" + type + "&page=1" ;
+ try{   
+    const response = await fetch(url);
+      const results = await response.json();
+        const items = await results.Search;
+        viewItems(await items);
+    }catch (error) {
+        console.log(error.message);
+      }
+    };
+  
 
 
 
 
-export const apiList = (s, type) => {
-    const url = BASE_URL + "s=" + s + "&type=" + type + "&page=1" + "&p";
-    fetch(url)
-      .then((response) => response.json())
-      .then((results) => {
-        const items = results.Search;
-        viewItems(items);
-      });
-  };
 
-
-
-
-
-  const viewItems = (items) => {
+  const viewItems =  (items) => {
     items.map((item) => {
      
 
@@ -159,28 +196,237 @@ export const apiList = (s, type) => {
     const pad15=document.createElement("div");
     pad15.className=pad15;
 
+    
+    
     pad15.appendChild(card);
     slide.appendChild(p);
+   
     
     
     
     slide.appendChild(title);
     slide.appendChild(card_body);
-    
     slide.appendChild(pad15);
     
-    slide.onclick = function() {
-        window.location.href = '';
-    };
-
     
+    /*slide.onclick = function() {
+        window.location.href = '';
+    };*/
+
+    /*creo il popup*/
+
+    /*const overl=document.createElement("div");
+    overl.id="overlay";
+    
+    const pop=document.createElement("div");
+    pop.id="popup";
+
+    const popupcontrols=document.createElement("div");
+    popupcontrols.className="popupcontrols";
+
+    const popupclose=document.createElement("span");
+    popupclose.className="popupclose";
+
+    const fa = document.createElement("i");
+    fa.className="fa-solid fa-x";
+    fa.style="height: 10px;padding: 10px;";
+
+    popupclose.appendChild(fa);
+    popupcontrols.appendChild(popupclose);
+    
+
+    const popupcontent = document.createElement("div");
+    popupcontent.className="popupcontent";
+
+    const h1 = document.createElement("h3");
+    const node3=document.createTextNode(item.Title);
+
+    h1.appendChild(node3);
+
+    popupcontent.appendChild(h1);
+    pop.appendChild(popupcontrols);
+    pop.appendChild(popupcontent);
+    
+    
+    document.body.appendChild(pop);*/
+
+
+    const overl=document.createElement("div");
+    overl.id="overlay";
+    
+    const pop=document.createElement("div");
+    pop.id="popup";
+    pop.style=(" border: 1px solid #d0d0d0;");
+
+    const popupcontrols=document.createElement("div");
+    popupcontrols.className="popupcontrols";
+
+    const popupclose=document.createElement("span");
+    popupclose.className="popupclose";
+    
+
+    const fa = document.createElement("button");
+    fa.className="fa-solid fa-x fa-2x active-bg;";
+    fa.style="width:fit-content; height: fit-content;max-height:10vh; padding: 10px; margin-left:10px;margin-top:2vh; border-radius: 50px;width:fit-content; position:absolute;background-color:#5bc0de";
+
+    popupclose.appendChild(fa);
+    popupcontrols.appendChild(popupclose);
+    
+
+    const popupcontent = document.createElement("div"); 
+    popupcontent.className="popupcontent";
+    popupcontent.style=("text-overflow:ellipsis");
+
+    const h1 = document.createElement("h3");
+    h1.style=("padding-top:10vh;");
+    const node3=document.createTextNode(item.Title);/*aggiungo titolo film*/
+
+
+    h1.appendChild(node3);
+  
+    
+    const testo=document.createElement("div");
+    testo.className="testopopup";
+    const node4=document.createTextNode("ANNO: " + item.Year );/*aggiungo anno film*/
+    const anno=document.createElement("p");/*creo uno break per andare a capo*/
+    anno.appendChild(node4);
+    testo.appendChild(anno);
+
+    /*chiamata api per il singolo film*/
+
+  
+    
+     const stampa1= async (items1)=>{
+      
+      
+      const node5=document.createTextNode("TRAMA: " + items1.Plot);/*aggiungo trama film*/
+      
+      const pl= document.createElement("p");
+      pl.appendChild(node5);
+      testo.appendChild(pl);
+
+      const node6=document.createTextNode("CAST: " + items1.Actors);/*aggiungo attori*/
+      
+      const ac= document.createElement("p");
+      ac.appendChild(node6);
+      testo.appendChild(ac);
+
+      const node7=document.createTextNode("DURATA: " + items1.Runtime);/*aggiungo durata*/
+      
+      const ru= document.createElement("p");
+      ru.appendChild(node7);
+      testo.appendChild(ru);
+
+      const node8=document.createTextNode("GENERE: " + items1.Genre);/*aggiungo genere*/
+      
+      const ge= document.createElement("p");
+      ge.appendChild(node8);
+      testo.appendChild(ge);
+
+      const node9=document.createTextNode("LINGUA: " + items1.Language);/*aggiungo genere*/
+      
+      const lan= document.createElement("p");
+      lan.appendChild(node9);
+      testo.appendChild(lan);
+
+    }
+
+    /* const apiListFilm1 = async (s1) => {
+      const url1 = BASE_URL + "i=" + s1 + "&plot=short" ;
+   try{   
+      const response1 = await fetch(url1);
+        const results1 = await response1.json();
+          const items1 = await results1;
+  
+          await stampa1(items1);
+          console.log(stampa1(items1));
+      }catch (error) {
+          console.log(error.message);
+        }
+      };*/
+
+      const apiListFilm1 = (s1) => {
+        const url1 = BASE_URL + "i=" + s1 + "&plot=short" ;
+        fetch(url1)
+        .then((response1)=>response1.json())
+        .then((results1)=>{
+          const items1=results1;
+            
+            stampa1(items1);
+          });
+        };
+    
+    (apiListFilm1(item.imdbID)); 
+    
+      
+      /****************************** */
+
    
+    popupcontent.appendChild(h1);
+    
+
+    popupcontent.appendChild(testo);
+   
+    pop.appendChild(popupcontrols);
+    pop.appendChild(popupcontent);
+    
+    
+    
+    document.body.appendChild(pop);
+
+  
+  /*var closePopup = document.getElementById("popupclose");*/
+  /*var overlay = document.getElementById("overlay");*/
+ /* var popup = document.getElementById("popup");*/
+   
+  slide.onclick=function() {
+    pop.style.display = 'block';
+    document.getElementById("modalOverlay").style.display = "block";
+  };
+
+  popupclose.onclick = function() {
+    overl.style.display = 'none';
+    pop.style.display = 'none';
+    document.getElementById("modalOverlay").style.display = "none";
+  };
+
   
     return slide;
   };
-  
-  
+
+
+ 
+  /*popup*/
+
+  var closePopup = document.getElementById("popupclose");
+  var overl = document.getElementById("overlay");
+  var popup = document.getElementById("popup");
+
+// When the user clicks on <div>, open the popup
+export const popUp=()=> {
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("show");
+  document.getElementById("modalOverlay").style.display = "block";
+
+}
+
+
+
+
  document.addEventListener('DOMContentLoaded',apiList("terminator", CONTENT_TYPE.MOVIE));
  document.addEventListener('DOMContentLoaded',apiList("star wars", CONTENT_TYPE.SERIES));
 
+
+ document.addEventListener('DOMContentLoaded', function() {
+ 
+
+   if(overl) {
+  overl.style.display = 'none';
+  popup.style.display = 'none';
+  $("html").remove('<div class="modalOverlay">');
+   }
+});
+
+  
+  
 
